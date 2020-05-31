ba9df2145a46796b71fa66695f21ad334c85e34c
LenaBystrykh <63150150+LenaBystrykh@users.noreply.github.com>
2020-05-31 15:38:56 +0500

    Add files via upload

---
+++ UP3/UP3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace UP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            bool res;
+            Console.WriteLine("Введите значение х");
+            x = CheckUserNumber();
+            Console.WriteLine("Введите значение y");
+            y = CheckUserNumber();
+            res = CheckDia(x, y);
+            if (res == true)
+                Console.WriteLine("Точка входит в заданный диапазон");
+            else Console.WriteLine("Точка не входит в заданный диапазон");
+
+        }
+        public static double CheckUserNumber()
+        {
+            bool ok;
+            double n;
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out n);
+                if (!ok) Console.WriteLine("Ошибка ввода. Повторите попытку");
+            } while (!ok);
+            return n;
+        }
+        public static bool CheckDia(double x, double y)
+        {
+            bool res;
+            if (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1)
+            {
+                res = true;
+            }
+            else res = false;
+            return res;
+        }
+    }
+}
